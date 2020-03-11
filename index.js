5682a3df8efbd20171959630d8a53027dfb23c00
Timtho <maciej.karasinski.95@gmail.com>
2020-03-11 19:43:52 +0100

    Added client side to production

--- index.js
+++ index.js
@@ -29,5 +29,16 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
+if (process.env.NODE_ENV === 'production') {
+  // Express will serve up production assets
+  app.use(express.static('client/build'));
+
+  // if Express doesn't recognize the route, it will serve index.html
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
