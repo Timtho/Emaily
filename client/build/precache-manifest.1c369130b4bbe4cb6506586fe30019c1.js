9f8c7d8b4dccf0f8b6b10f9892eda543f1cdac02
Timtho <maciej.karasinski.95@gmail.com>
2020-03-11 20:14:07 +0100

    fixing versions

---
+++ client/build/precache-manifest.1c369130b4bbe4cb6506586fe30019c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e038155d5eb74cc367b13dfe6ad88e5c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8949cc9694c7ea7d166a",
+    "url": "/static/css/2.36020323.chunk.css"
+  },
+  {
+    "revision": "8949cc9694c7ea7d166a",
+    "url": "/static/js/2.acd92ea1.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.acd92ea1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e16cb340e422a91e59a0",
+    "url": "/static/js/main.923ce7f2.chunk.js"
+  },
+  {
+    "revision": "08a01cac7a9424eef63f",
+    "url": "/static/js/runtime-main.ef76e641.js"
+  }
+]);
